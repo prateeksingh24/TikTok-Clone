35ccf9f33622244e47ac62253cb5b5e14644c37a
prateeksingh24 <prateeksingh2423@gmail.com>
2024-02-19 03:22:30 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
-import 'package:tiktok_clone/view/screens/auth/login_screen.dart';
+import 'package:tiktok_clone/controller/auth_controller.dart';
+import 'package:tiktok_clone/view/screens/auth/signup_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((value){
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -12,14 +23,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
             useMaterial3: false,
       ),
-      home: LoginScreen(),
+      home: SignUpScreen(),
     );
   }
 }
